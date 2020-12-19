e0775a07d960292d558b12655217c354cfa58f7d
jenniferxfinsen <jenniferxfinsen@gmail.com>
2020-12-20 01:19:03 +0700

    update landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-import snowman from './snowman.png';
+import tree from './assets/tree.png'
+import christmas from './assets/christmas-tree-2561029.png'
+import qr from './assets/qr.png'
+import gift from './assets/gift.png'
+import santa from './assets/santa.png'
+import snowman from './assets/snowman.png'
 import './App.css'
 import './App.css';
 
@@ -7,15 +12,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="background">
+        <div className="background" style={{marginBottom: 24}}>
           <h1 className="title">Jennifer x Finsen</h1>
-          <img src={snowman} className="App-logo" alt="snowman"></img>
+          {/* <img src={snowman} className="App-logo" alt="snowman"></img> */}
         </div>
-        <p className="text">you are one more step closer to the surprise</p>
-        <div className="btn-container">
+        {/* <p className="text">you are one more step closer to the surprise</p> */}
+        <div className="main-event" style={{background: '#76C79E'}}>
+            <img src={tree} style={{width: 200, height: 150}}></img>
+        </div>
+        <div className="menu-container">
+          <div className="menu" style={{background: '#9BA3FF'}}>
+            <img src={snowman} style={{width: 170, height: 130}}></img>
+          </div>
+          <div className="menu" style={{background: '#F05D48'}}>
+            <img src={christmas} style={{width: 170, height: 130}}></img>
+          </div>
+          <div className="menu" style={{background: '#F4A17C'}}>
+            <img src={gift} style={{width: 170, height: 130}}></img>
+          </div>
+          <div className="menu" style={{background: '#FFCF86'}}>
+            <img src={santa} style={{width: 170, height: 130}}></img>
+          </div>
+          <div className="menu" style={{background: '#D9A5B5'}}>
+            <h2>Scan Me {'>'}</h2>
+          </div>
+          <div className="menu" style={{background: '#4E5567'}}>
+            <img src={qr} style={{width: 150, height: 150}}></img>
+          </div>
           
-            <button className="btn-next">Next</button>
-       
         </div>
       </header>
     </div>
