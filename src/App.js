ac102bda190a792aed01cd5ac5cde715abb345fb
jenniferxfinsen <jenniferxfinsen@gmail.com>
2020-12-20 00:29:14 +0700

    fix landing page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { BrowserRouter, Switch, Route, Link, Router } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import snowman from './snowman.png';
+import './App.css'
 import './App.css';
-import StartPage from './Start/Mobile'
-import Dashboard from './Dashboard/Mobile'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/start" render={() => <StartPage />}></Route>
-        <Route path="/dashboard" render={() => <Dashboard />}></Route>
-      </Switch>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <div className="background">
+          <h1 className="title">Jennifer x Finsen</h1>
+          <img src={snowman} className="App-logo" alt="snowman"></img>
+        </div>
+        <p className="text">you are one more step closer to the surprise</p>
+        <div className="btn-container">
+          
+            <button className="btn-next">Next</button>
+       
+        </div>
+      </header>
+    </div>
   );
 }
 
